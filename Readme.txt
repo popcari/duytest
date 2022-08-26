b3a1c5e5d21f5de3506f12b9f3458700cfc05efd
duy.ntHachix <107833418+duyhachix@users.noreply.github.com>
2022-08-26 18:00:21 +0700

    day 26/8

---
+++ Readme.txt
@@ -0,0 +1,17 @@
+date 25/8
+1. nhập 1 mảng các số nguyên
+  - tách mảng thành mảng số chẵn và số lẽ
+2. Tính tổng, hiệu, tích, thương của 2 số
+-> làm tất cả làm trên 1 web
+
+date 26/8
+3. nhập 3 số là chiều dài 3 cạnh của tam giác
+- tính diện tích 
+- kiểm tra xem đó là tam giác gì 
+4. nhập vào 1 chuỗi, 
+- in ra độ dài mà ko dùng hàm tiện ích
+- in ra viết hoa, viết thường
+- in hoa ở đầu từ 
+- biến 1 chuỗi thành camel case (dạng như onClick) và snake case
+
+# bai3: nhập dãy số, sắp xếp dãy số theo chiều tăng dần, giảm dần (k dùng các hàm built-in)
--- main.js
+++ main.js
@@ -19,14 +19,11 @@ function splitEvenOdd() {
 
 // bai 2: tinh tong hieu cua 2 so
 
-var num1 = Number(document.getElementById('firstnumber').value);
-var num2 = Number(document.getElementById('secondnumber').value);
-console.log({ ngoaiham1: num1 });
-console.log({ ngoaiham2: num2 });
 function getPlus() {
+	var num1 = Number(document.getElementById('firstnumber').value);
+	var num2 = Number(document.getElementById('secondnumber').value);
 	let plusResult = num1 + num2;
-	console.log({ trongham1: num1 });
-	console.log({ trongham2: num2 });
+
 	document.getElementById('result').value = plusResult;
 }
 function getMinute() {
@@ -46,4 +43,91 @@ function getDivide() {
 	let num2 = Number(document.getElementById('secondnumber').value);
 	let divideResult = Math.round((num1 / num2) * 100) / 100;
 	document.getElementById('result').value = divideResult;
+}
+
+// leeson 3: tinh dien tich tam giac biet 3 canh
+function onCalculate() {
+	let a = Number(document.getElementById('aTriangle').value);
+	let b = Number(document.getElementById('bTriangle').value);
+	let c = Number(document.getElementById('cTriangle').value);
+	if (a + b < c || b + c < a || c + a < b) {
+		alert('ABC không phải là hình tam giác');
+	} else {
+		if ((a === b) & (b === c)) {
+			alert('ABC là tam giác đều');
+		} else if (a == b || b == c || c == a) {
+			if (
+				a * a == b * b + c * c ||
+				b * b == a * a + c * c ||
+				c * c == a * a + b * b
+			) {
+				alert('ABC là tam giác vuông cân');
+			}
+			alert('ABC là tam giác cân');
+		} else if (
+			a * a == b * b + c * c ||
+			b * b == a * a + c * c ||
+			c * c == a * a + b * b
+		) {
+			alert('ABC là tam giác vuông ');
+		} else if (
+			a * a > b * b + c * c ||
+			b * b > a * a + c * c ||
+			c * c > a * a + b * b
+		) {
+			alert('ABC là tam giác tù');
+		}
+	}
+	let halfArea = (a + b + c) / 2;
+	// console.log(halfArea);
+	let triangleArea =
+		Math.round(
+			Math.sqrt(
+				halfArea * (halfArea - a) * (halfArea - b) * (halfArea - c)
+			) * 10
+		) / 10;
+	console.log(triangleArea);
+	document.getElementById('result_3').value = triangleArea;
+}
+
+// bai4
+function strLength() {
+	let myString = document.getElementById('string').value;
+	// console.log(myString);
+	let length = 0;
+	// console.log(length);
+	while (myString[length] !== undefined) {
+		length++;
+		return length;
+	}
+	console.log('do dai string: ', length);
+	document.getElementById('result_4').value = length;
+}
+function strUpper() {
+	let myString = document.getElementById('string').value.toUpperCase();
+	console.log(myString);
+	document.getElementById('result_4').value = myString;
+}
+function strLower() {
+	let myString = document.getElementById('string').value.toLowerCase();
+	console.log(myString);
+	document.getElementById('result_4').value = myString;
+}
+function capitalizeFirstLetter() {
+  let myString = document.getElementById('string').value;
+  // change myString thành 1 Array chứa các word 
+  let newArray = myString.split(' ');
+  // change các chữ csai đầu của array mới tạo thành in hoa
+	for (let i = 0; i < newArray.length; i++) {
+		newArray[i] = newArray[i][0].toUpperCase() + newArray[i].slice(1);
+	}
+  console.log('array sau khi split: ', newArray);
+  // sau khi in hoa các chữ cái đầu thì ghép lại thành newString
+	let newString = newArray.join(' ');
+	console.log('new string là: ', newString);
+	// console.log('fdasfsda', newArray);
+	document.getElementById('result_4').value = newString;
+}
+function camelCase() {
+  
 }
